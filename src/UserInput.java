2865026ea56b8d00c2c1ec727e8279e8d5eaaaa5
monica-jftw <monica.mittal@justforthewin.com>
2019-11-26 16:23:36 +0100

    UserInput class code Refactored !

--- src/UserInput.java
+++ src/UserInput.java
@@ -16,40 +16,34 @@ public class UserInput {
         System.out.println("Enter 1, 2, 3 or 4 (0 for exit): ");
 
         // i want to make a list or Array??? of choices = {1,2,3,4}
-        List<Integer> myList = new ArrayList<Integer>();
-        myList.add(1);
-        myList.add(2);
-        myList.add(3);
-        myList.add(4);
+        //List<Integer> myList = new ArrayList<Integer>();
+        //myList.add(1);
+        //myList.add(2);
+        //myList.add(3);
+        //myList.add(4);
 
-        // if (userinput is not found in options),throw error and ask again to userinput; we can use while loop here on list)
-        int option;
-        // while(myList.hasoption)
-        while (!true){
-            System.out.println("Please choose from the given options");
-        //Similarly i want to put a check if user enters as 'one', for this check if(userinput is numeric), throw error and ask again ! Use parser !
-        // or should i just use IO exception and not use it as a check cond !
-        if(!numberOrNot(true)){
-            System.out.println("Please enter the number in numeric form only");
-            if(option == 1){
-            calculator.addNumbers();
-        }else if(option == 2){
-                calculator.subtractNumbers();
-            }else if(option == 3){
-                calculator.multiplyNumbers();
-        }else if(option == 4){
-                calculator.divideNumbers();
+        while (below methods are true) {
+            // if (userinput is found in options),throw error and ask again to userinput; we can use while loop here on list)
+            //when conditions meet, Validate the user input.
+        }
+            if (!numberOrNot(true)) {
+            }
+        }
+        public boolean isFound(option){
+        // Check if option is not from the list i.e.1,2,3,4)
+        System.out.println("Please choose from the given options");
+        return true;
         }
 
-    public boolean numberOrNot(option) {
+        public boolean numberOrNot(option) {
+            //check if user enters as 1 and not 'one' ! If so, print error
         try {
             Integer.parseInt(option);
         } catch (NumberFormatException ex) {
+        }
+            System.out.println("Please enter the number in numeric form only");
             return false;
         }
-        return true;
-        }
-    }
 
     }
 
